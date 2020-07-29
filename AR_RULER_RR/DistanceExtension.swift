eb2e03c2f67991d8c94b2964067d9e6da8145835
Ciro Angeleri <ciroangeleri@gmail.com>
2020-07-29 10:19:11 -0300

    First commit

---
+++ AR_RULER_RR/DistanceExtension.swift
@@ -0,0 +1,15 @@
+//
+//  DistanceExtension.swift
+//  AR_RULER_RR
+//
+//  Created by Ciro Angeleri on 28/07/2020.
+//  Copyright © 2020 Ciro Angeleri. All rights reserved.
+//
+
+import Foundation
+
+extension SCNNode {
+    func distance(to destination: SCNNode) -> CGFloat {
+        // Your code goes here
+    }
+}
--- AR_RULER_RR/ViewController.swift
+++ AR_RULER_RR/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 import SceneKit
 import ARKit
 
+extension SCNNode {
+    
+    func distance(to destination: SCNNode) -> CGFloat {
+        
+        let dx = destination.position.x - position.x
+        let dy = destination.position.y - position.y
+        let dz = destination.position.z - position.z
+         
+        let meters = sqrt(dx*dx + dy*dy + dz*dz)
+         
+        return CGFloat(meters * 100)
+    }
+
+}
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    // Label to show measures
+    var measurementLabel = UILabel()
+    
+    var spheres: [SCNNode] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +43,99 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        measurementLabel.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 100)
+        measurementLabel.backgroundColor = .gray
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        measurementLabel.text = "0 centimetros"
+        measurementLabel.textAlignment = .center
+        view.addSubview(measurementLabel)
+        
+        // Creates a tap handler and then sets it to a constant
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        
+        // Sets the amount of taps needed to trigger the handler
+        tapRecognizer.numberOfTapsRequired = 1
+        
+        // Adds the handler to the scene view
+        sceneView.addGestureRecognizer(tapRecognizer)
+        
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        // Gets the location of the tap and assigns it to a constant
+        let location = sender.location(in: sceneView)
+         
+        // Searches for real world objects such as surfaces and filters out flat surfaces
+        let hitTest = sceneView.hitTest(location, types: [ARHitTestResult.ResultType.featurePoint])
+         
+        // Assigns the most accurate result to a constant if it is non-nil
+        guard let result = hitTest.last else { return }
+        
+        // Converts the matrix_float4x4 to an SCNMatrix4 to be used with SceneKit
+        let transform = SCNMatrix4.init(result.worldTransform)
+         
+        // Creates an SCNVector3 with certain indexes in the matrix
+        let vector = SCNVector3Make(transform.m41, transform.m42, transform.m43)
+         
+        // Makes a new sphere with the created method
+        let sphere = newSphere(at: vector)
+        
+        if let first = spheres.first {
+            // Adds a second sphere to the array
+            spheres.append(sphere)
+            var distance = sphere.distance(to: first)
+            
+            measurementLabel.text = "\(distance) centimetros"
+             
+            // If more that two are present...
+            if spheres.count > 2 {
+                 
+                // Iterate through spheres array
+                for sphere in spheres {
+                     
+                    // Remove all spheres
+                    sphere.removeFromParentNode()
+                }
+                 
+                // Remove extraneous spheres
+                spheres = [spheres[2]]
+            }
+        } else {
+            // Add the sphere
+            spheres.append(sphere)
+        }
+        
+        // Iterate through spheres array
+        for sphere in spheres {
+             
+            // Add all spheres in the array
+            self.sceneView.scene.rootNode.addChildNode(sphere)
+        }
+    }
+    
+    func newSphere(at position: SCNVector3) -> SCNNode {
+        // Creates an SCNSphere with a radius of 0.01 meters
+        let sphere = SCNSphere(radius: 0.01)
+         
+        // Converts the sphere into an SCNNode
+        let node = SCNNode(geometry: sphere)
+         
+        // Positions the node based on the passed in position
+        node.position = position
+        
+        // Creates a material that is recognized by SceneKit
+        let material = SCNMaterial()
+        
+        // Converts the contents of the PNG file into the material
+        material.diffuse.contents = UIColor.orange
+        
+        // Creates realistic shadows around the sphere
+        material.lightingModel = .blinn
+         
+        // Wraps the newly made material around the sphere
+        sphere.firstMaterial = material
+        
+        return node
     }
     
     override func viewWillAppear(_ animated: Bool) {
